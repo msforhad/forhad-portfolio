58f9d6a66105569e6ecf63e22007cdee9f1b3925
msforhad <msforhad717@gmail.com>
2025-04-09 01:32:22 +0600

    update

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/forhad-portfolio/',
+  base: '/forhad-portfolio',
   plugins: [
     tailwindcss(),
     react()
